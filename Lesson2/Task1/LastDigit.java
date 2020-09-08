9903c0671a01acad19608556f90de46974e7a2f2
abilashenko <a.bilashenko@x5.ru>
2020-09-08 19:13:08 +0300

    Lesson1

---
+++ Lesson2/Task1/LastDigit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LastDigit {
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int lastdigit = number%10;
+        String result = "Последняя цифра вашего числа: " + lastdigit;
+        System.out.println (result);
+    }
+}
