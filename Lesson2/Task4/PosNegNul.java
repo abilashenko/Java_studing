1be3972683aa3a4d7c201f59d93ef80269b143e4
abilashenko <a.bilashenko@x5.ru>
2020-09-09 12:54:21 +0300

    upd

--- Lesson2/Task4/PosNegNul.java
+++ Lesson2/Task4/PosNegNul.java
@@ -1,20 +1,21 @@
-package Lesson2.Task3;
+package Lesson2.Task4;
 
 import java.util.Scanner;
 
-public class PosNeg {
+public class PosNegNul {
     public static void main(String[] args) {
         int number;
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        while(true) {
             System.out.println("Введите целое число:");
             while (!scanner.hasNextInt()) {
                 System.out.println("Вы ввели не целое число!\nВведите целое число:");
                 scanner.next();
             }
             int input = scanner.nextInt();
-            if (input > 0) number = input + 1;
-            else number = input;
+            if (input > 0) {number = input + 1;}
+            else if (input == 0) {number = 10;}
+            else {number = input - 2;}
             String result = "Результат вычислений:" + (number);
             System.out.println(result);
         }
