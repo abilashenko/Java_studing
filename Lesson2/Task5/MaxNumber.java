c0f8f3950f1de461b6a9365b1fa99ae3c0cd9be5
abilashenko <a.bilashenko@x5.ru>
2020-09-09 21:49:48 +0300

    update

--- Lesson2/Task5/MaxNumber.java
+++ Lesson2/Task5/MaxNumber.java
@@ -5,30 +5,20 @@ import java.util.Scanner;
 
 public class MaxNumber {
         public static void main(String[] args) {
-            int n1;
-            int n2;
-            int n3;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите первое целое число:");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Вы ввели не целое число!\nВведите первое целое число:");
-                    scanner.next();
+                int[] Arr = new int[3];
+                Scanner scanner = new Scanner(System.in);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    System.out.print("Введите " + (i + 1) + "-е целое число: ");
+                    while (!scanner.hasNextInt())
+                        {
+                        System.out.println("Вы ввели не целое число!");
+                        scanner.next();
+                        }
+                    Arr[i] = scanner.nextInt();
                 }
-                n1=scanner.nextInt();
-                System.out.println("Введите второе целое число:");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Вы ввели не целое число!\nВведите второе целое число:");
-                    scanner.next();
-                }
-                n2=scanner.nextInt();
-                System.out.println("Введите третье целое число:");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Вы ввели не целое число!\nВведите третье целое число:");
-                    scanner.next();
-                }
-                n3=scanner.nextInt();
-                int[] Arr ={n1, n2, n3};
-                Arrays.sort(Arr);
-                System.out.println("Наименьшее число из трёх введённых: " + Arr[0]);
-            }
+            Arrays.sort(Arr);
+            System.out.println("Наименьшее число из трёх введённых: " + Arr[0]);
         }
+}
