1798f81085567854883f0e5d09830c333eaf3078
abilashenko <a.bilashenko@x5.ru>
2020-09-11 00:44:27 +0300

    Update

--- Lesson2/Task1/LastDigit.java
+++ Lesson2/Task1/LastDigit.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class LastDigit {
     public static void main(String[] args) {
-        System.out.println("Введите число: ");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Вы ввели не целое число!\nВведите целое число:");
+            scanner.next();
+        }
         int number = scanner.nextInt();
         int lastdigit = number%10;
         String result = "Последняя цифра вашего числа: " + lastdigit;
