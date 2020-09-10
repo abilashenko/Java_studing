ea9526d6e0d993a57de82f8672fc183090e38a7f
abilashenko <a.bilashenko@x5.ru>
2020-09-11 00:35:33 +0300

    task10

---
+++ Lesson2/Task10/ArrNulEnd.java
@@ -0,0 +1,19 @@
+package Lesson2.Task10;
+
+import java.util.Arrays;
+
+public class ArrNulEnd {
+    public static void main(String[] args) {
+        int[] Arr = {15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52};
+        int len = Arr.length;
+        int[] newArr = new int[len];
+        int i=0;
+        for (int element : Arr) {
+            if (element != 0) {
+                newArr[i] = element;
+                i++;
+            }
+        }
+        System.out.println("Новый массив: " + Arrays.toString(newArr));
+    }
+}
