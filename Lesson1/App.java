036f9d6a2d1c563cedf10c023da3614dab8e9c9f
abilashenko <a.bilashenko@x5.ru>
2020-09-16 14:05:19 +0300

    upd

--- Lesson1/App.java
+++ Lesson1/App.java
@@ -1,6 +1,7 @@
+package Lesson1;
+
 public class App {
-	public static void main(String[] args) {
-		System.out.println ("Hello X5-Technologies");
-	}
-	
+    public static void main(String[] args) {
+        System.out.println("Hello X5-Technologies");
+    }
 }
