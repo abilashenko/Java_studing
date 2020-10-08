ce5a5e44680d265c519621f3525fadeec4724090
abilashenko <a.bilashenko@x5.ru>
2020-10-08 11:43:20 +0300

    Lesson6.Task1

--- Lesson6/Task1/LinkedClass.java
+++ Lesson6/Task1/LinkedClass.java
@@ -1,4 +1,129 @@
 package Lesson6.Task1;
 
-public class LinkedClass {
+public class LinkedClass<E> {
+    private class Node {
+        private Node next;
+        private Node prev;
+        private final E object;
+
+        public Node(E object) {
+            this.object = object;
+            next = null;
+            prev = null;
+        }
+    }
+
+    private int counter = 0;
+    private Node first = null;
+    private Node last = null;
+
+    public int size() {
+        return counter;
+    }
+
+    public void add(E item) {
+        if (first == null) {
+            first = new Node(item);
+            last = first;
+        } else {
+            Node newNode = new Node(item);
+            newNode.prev = last;
+            last.next = newNode;
+            last = newNode;
+        }
+        counter++;
+    }
+
+    public void remove(int index) {
+        int listIndex = 0;
+        Node element = first;
+        while (true) {
+            if (listIndex == index) {
+                if (element == first) {
+                    first = element.next;
+                    first.prev = null;
+                } else if (element == last) {
+                    last = element.prev;
+                    last.next = null;
+                } else {
+                    element.prev.next = element.next;
+                    element.next.prev = element.prev;
+                }
+                counter--;
+                break;
+            } else {
+                listIndex++;
+                element = element.next;
+            }
+        }
+    }
+
+    public void remove(E item) {
+        if (counter == 0) {
+            return;
+        }
+        Node element = first;
+        while (true) {
+            if ((element.object != null) && (element.object.equals(item))) {
+                if (element == first) {
+                    first = element.next;
+                    first.prev = null;
+                } else if (element == last) {
+                    last = element.prev;
+                    last.next = null;
+                } else {
+                    element.prev.next = element.next;
+                    element.next.prev = element.prev;
+                }
+                counter--;
+                break;
+            } else {
+                element = element.next;
+                if (element == null) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public E get(int index) {
+        int indx = 0;
+        Node element = first;
+        while (true) {
+            if (indx == index) {
+                return element.object;
+            } else {
+                indx++;
+                element = element.next;
+            }
+        }
+    }
+
+    public void clear() {
+        first = null;
+        last = null;
+        counter = 0;
+    }
+
+    @Override
+    public String toString() {
+        String list;
+        if (counter == 0) {
+            list = "{}";
+        } else {
+            list = "{";
+            Node element = first;
+            while (true) {
+                if (element.object != null) {
+                    list += element.object.toString() + ",";
+                }
+                element = element.next;
+                if (element == null) {
+                    break;
+                }
+            }
+            list += "}";
+        }
+        return list;
+    }
 }
--- Lesson6/Task1/MainClass.java
+++ Lesson6/Task1/MainClass.java
@@ -3,6 +3,7 @@ package Lesson6.Task1;
 public class MainClass {
     public static void main(String[] args) {
         ArrayClass<String> arrayClass = new ArrayClass<>();
+
         System.out.println("Текущий размер коллекции: " + arrayClass.size());
 
         arrayClass.add("KTM");
@@ -15,7 +16,7 @@ public class MainClass {
         System.out.println("Добавляем в коллекцию элементы. Коллекция: " + arrayClass.toString());
         System.out.println("Теперь размер коллекции: " + arrayClass.size());
 
-        arrayClass.remove(2);
+        arrayClass.remove(1);
         System.out.println("Из коллекции удалили 2-й элемент : " + arrayClass.toString());
 
         arrayClass.remove("KTM");
@@ -25,5 +26,34 @@ public class MainClass {
 
         arrayClass.clear();
         System.out.println("Коллекция очищена: " + arrayClass.toString());
+
+        System.out.println();
+        System.out.println("=============================================================");
+        System.out.println();
+
+        LinkedClass<String> linkedClass = new LinkedClass<>();
+
+        System.out.println("Текущий размер списка: " + linkedClass.size());
+
+        linkedClass.add("BMW");
+        linkedClass.add("Audi");
+        linkedClass.add("Mercedes");
+        linkedClass.add("Toyota");
+        linkedClass.add("Nissan");
+        linkedClass.add("Skoda");
+
+        System.out.println("Добавляем в список элементы. Список: " + linkedClass.toString());
+        System.out.println("Теперь размер списка: " + linkedClass.size());
+
+        linkedClass.remove(1);
+        System.out.println("Из списка удалили 2-й элемент : " + linkedClass.toString());
+
+        linkedClass.remove("Nissan");
+        System.out.println("Из списка удалили элемент 'Nissan': " + linkedClass.toString());
+
+        System.out.println("Первый элемент списка: " + linkedClass.get(0));
+
+        linkedClass.clear();
+        System.out.println("Список очищен: " + linkedClass.toString());
     }
 }
